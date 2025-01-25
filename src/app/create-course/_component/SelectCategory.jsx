d8c78a5f42693e2d1e08066c6e06d1a474f8a266
olamiji-8 <olamijisotomi@gmail.com>
2025-01-25 21:38:35 +0100

    updated

--- src/app/create-course/_component/SelectCategory.jsx
+++ src/app/create-course/_component/SelectCategory.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CategoryList from '../../_shared/CategoryList'
 import Image from 'next/image'
+import { UserInputContext } from '../../_context/UserInputContext'
 
 const SelectCategory = () => {
+    const {userCourseInput, setUserCourseInput}= useContext(UserInputContext)
+
+    const handleCatgoryChange=(category)=>{
+        setUserCourseInput(prev=>({
+            ...prev,
+            category:category
+        }))
+    }
   return (
     <div className='px-10 md:px-20'>
          <h2 className='my-5'>Select the Course Category</h2>
     
     <div className='grid grid-cols-3 gap-10'>
       {CategoryList.map((item, index)=>{
-        <div className='flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-secondary cursor-pointer'>
+        <div className={`flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-secondary cursor-pointer ${userCourseInput?.category==item.name&& "border-primary bg-primary"}`} onClick={()=>handleCatgoryChange(item.name)}>
             <Image src={item.icon} width={50} height={50}/>
             <h2>{item.name}</h2>
         </div>
--- src/app/create-course/layout.jsx
+++ src/app/create-course/layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Header from '../dashboard/_components/Header'
+import { UserInputContext } from '../_context/UserInputContext'
 
 const CreateCourseLayout = ({children}) => {
+  const [userCourseInput, setUserCourseInput]= useState([])
   return (
     <div>
+      <UserInputContext.Provider value={{userCourseInput, setUserCourseInput}}> 
       <Header/>
       {children}
+   
+    </UserInputContext.Provider>
     </div>
   )
 }
